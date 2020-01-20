2d4a34f17f37de5f7787fda7ecf0cf2bcf74df3a
aisy <aisymuhammad@gmail.com>
2020-01-20 16:59:44 +0700

    rename name and add some code style

---
+++ src/components/commons/AppBarWeb.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+function AppBarWeb() {
+  return (
+    <AppBar
+      position={"static"}
+    >
+      <Toolbar>
+        <Typography className={useStyle.title} variant={"h6"}>
+          Sample cart
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+const useStyle = makeStyles(theme => ({
+  title: {
+    display: "none",
+  }
+}))
+
+export default AppBarWeb

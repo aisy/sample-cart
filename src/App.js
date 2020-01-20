c5b27f4c8407e4bb33f3b668142421f169a55784
aisy <aisymuhammad@gmail.com>
2020-01-20 19:44:09 +0700

    add AppBarWeb components to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import AppBarWeb from './components/commons/AppBarWeb';
+
 
 function App() {
   return (
     <>
+      <AppBarWeb />
       Here's contents
     </>
   );

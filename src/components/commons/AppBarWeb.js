737d7368cc50d315ce4e7be9e32752e4a1b5707a
aisy <aisymuhammad@gmail.com>
2020-01-20 19:43:47 +0700

    add style from material-ui

--- src/components/commons/AppBarWeb.js
+++ src/components/commons/AppBarWeb.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
 function AppBarWeb() {
   return (

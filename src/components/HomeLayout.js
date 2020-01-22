55dc8d6b3f8cb2390e60214ab0896e2cbddb5ff2
aisy <aisymuhammad@gmail.com>
2020-01-22 16:46:09 +0700

    add async to make data await and add loading

--- src/components/HomeLayout.js
+++ src/components/HomeLayout.js
@@ -1,33 +1,56 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
-import FilterItems from './commons/Home/FilterItems';
-import CardItem from './commons/Home/CardItem';
+import FilterItems from './Home/FilterItems';
+import CardItem from './Home/CardItem';
+
+import { FilterPriceContext } from '../contexts/FilterPrice_Context';
 
 const HomeLayout = (props) => {
 
-  const listItems = props.items;
+  let { setFilteredItem, filteredItem } = useContext(FilterPriceContext);
+  const [ loading, setLoading ] = useState(true);
+
+  const setData = async () => {
+    let listItems = await props.items;
+    setFilteredItem(listItems);
+    setLoading(false);
+  }
 
   useEffect(() => {
-    console.log(listItems)
-  }, [ listItems ]);
+    setData();
+    console.log(filteredItem);
+  }, [ filteredItem ]);
+
+  const ThePage = () => (
+    <div>
+      <FilterItems />
+      <Grid container spacing={2}>
+        {
+          filteredItem.map(items => {
+            return (
+              <CardItem
+                key={items.id}
+                itemName={items.name_item}
+                price={items.price}
+              />
+            )
+          })
+        }
+      </Grid>
+    </div>
+  );
 
   return (
     <Container
       maxWidth={"lg"}
       style={{ padding: 50 }}
     >
-      <FilterItems />
-      <Grid container spacing={2}>
-        {
-          listItems.map(items => {
-            return (
-              <CardItem key={items.id} />
-            )
-          })
-        }
-      </Grid>
+      {
+        loading ? <CircularProgress /> : <ThePage />
+      }
     </Container>
   );
 }

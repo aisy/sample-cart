62143b1f0daa3c2a79bb404493a44963872044c5
aisy <aisymuhammad@gmail.com>
2020-01-22 09:23:34 +0700

    add new component HomeLayout

---
+++ src/components/HomeLayout.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+
+import CardItem from './commons/CardItem';
+
+function App() {
+  return (
+    <Container
+      maxWidth={"lg"}
+      style={{ padding: 50 }}
+    >
+      <Grid container spacing={5}>
+        <CardItem />
+        <CardItem />
+        <CardItem />
+        <CardItem />
+        <CardItem />
+        <CardItem />
+      </Grid>
+    </Container>
+  );
+}
+
+export default App;

95e634b9cf75b50763d582fd9829437b620b17f7
aisy <aisymuhammad@gmail.com>
2020-01-22 09:11:21 +0700

    remove CardMedia and add Grid item

--- src/components/commons/CardItem.js
+++ src/components/commons/CardItem.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
 
 function CardItem() {
   const classes = stylesCard();
@@ -17,27 +17,29 @@ function CardItem() {
   }
 
   return (
-    <Card className={classes.card}>
-      <div className={classes.dummy} />
-      <CardContent>
-        <Typography gutterBottom variant={"subtitle1"}>
-          <Box fontWeight={600}>
-            {"Apple iPad 7 2019 / iPad 10.2 Inch Wifi Only 32GB Gold Grey Silver".substr(0, 20) + "..."}
-          </Box>
-        </Typography>
-        <Typography gutterBottom variant={"subtitle1"} className={classes.price}>
-          <Box fontWeight={700}>
-            {rupiahConvert(800000)}
-          </Box>
-        </Typography>
-      </CardContent>
-    </Card>
+    <Grid item>
+      <Card className={classes.card}>
+        <div className={classes.dummy} />
+        <CardContent>
+          <Typography gutterBottom variant={"subtitle1"}>
+            <Box fontWeight={600}>
+              {"Apple iPad 7 2019 / iPad 10.2 Inch Wifi Only 32GB Gold Grey Silver".substr(0, 20) + "..."}
+            </Box>
+          </Typography>
+          <Typography gutterBottom variant={"subtitle1"} className={classes.price}>
+            <Box fontWeight={700}>
+              {rupiahConvert(800000)}
+            </Box>
+          </Typography>
+        </CardContent>
+      </Card>
+    </Grid>
   )
 }
 
 const stylesCard = makeStyles({
   card: {
-    maxWidth: 200,
+    maxWidth: 400,
     marginBottom: 20,
   },
   media: {
